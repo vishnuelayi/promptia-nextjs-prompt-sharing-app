bdf671d0a80f0f38824d888e4ebd8c28dd29a86d
Vishnu Elayi <vishnuelayi@gmail.com>
2024-03-11 15:26:56 +0530

    just introduced the homepage and the mobile view dropdown

---
+++ app/layout.jsx
@@ -0,0 +1,25 @@
+import Nav from "@components/Nav";
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "Promptia",
+  description: "Discover & Share AI Prompts",
+};
+
+function RootLayout({children}) {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <main className="app">
+        <Nav/>
+        {children}</main>
+      </body>
+    </html>
+  );
+}
+
+export default RootLayout;
